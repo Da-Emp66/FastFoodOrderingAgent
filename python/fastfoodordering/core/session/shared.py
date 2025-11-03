696fe1af412b7c8d4721ead8d1ca41eb950c8c7f
Da-Emp66 <asherbean10@gmail.com>
2025-11-02 19:07:34 -0500

    forgot to add shared.py

---
+++ python/fastfoodordering/core/session/shared.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from typing import Dict
+import docker
+from docker.models.containers import Container
+from core.session.session import SessionManager
+
+
+docker_client = docker.from_env()
+session_manager = SessionManager(Path(__file__).parent.parent.parent / "configuration" / "session_manager.yaml")
+session_ids_to_containers: Dict[str, Container] = {}
