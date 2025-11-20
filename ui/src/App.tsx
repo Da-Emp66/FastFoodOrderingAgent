6e11c6aace4dd086a098b12a05145d9af0b128e2
Eduardo Bourget <eduardobourget@gmail.com>
2025-11-19 21:41:39 -0500

    mute button implemented

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import SignIn from './components/SignIn';
 import HungryInput from './components/HungryInput';
 import VoiceInterface from './components/VoiceInterface';
-// import TTS from './components/TTS'; 
+// import TTS from './components/TTS';
 
 type AppState = "signin" | "hungry" | "voice";
 
